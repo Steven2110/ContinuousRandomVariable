1d0670ae082b40db5e0ee4a6c1b520cedef62be7
Steven Wijaya <steven.wijaya2001@gmail.com>
2023-05-11 23:22:09 +0700

    Initial Commit

---
+++ ContinuousRandomVariable/ContinuousRandomVariableApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContinuousRandomVariableApp.swift
+//  ContinuousRandomVariable
+//
+//  Created by Steven Wijaya on 11.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct ContinuousRandomVariableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
